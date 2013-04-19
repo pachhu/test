55be432db595ae1c87851a5954f4e1a23ad29158
Prashanth Gurunath Shivakumar <pgurunat@usc.edu>
2013-04-18 20:00:29 -0700

    Added modified Bandit_test

--- build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.em
-
-export CATKIN_TEST_RESULTS_DIR="/home/prashanth/groovy_workspace/sandbox/bandit_test/build/test_results"
-export ROS_TEST_RESULTS_DIR=$CATKIN_TEST_RESULTS_DIR
